247218ed56868f384b84a69a64b6fdb175a4a0db
Chul Han <hch728@nate.com>
2024-09-17 10:55:14 +0900

    commit

---
+++ src/main/java/com/example/demo/controller/UserInfoController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.UserInfoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/userInfo")
+public class UserInfoController {
+
+    private final UserInfoService userInfoService;
+
+    public UserInfoController(UserInfoService userInfoService) {
+        this.userInfoService = userInfoService;
+    }
+
+    @GetMapping("/getUserInfo")
+    public Map<String, Object> getUserInfo(@RequestParam Long id) {
+        return userInfoService.getUserInfo(id);
+    }    
+}
